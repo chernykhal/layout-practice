76559b154723f419649afcdb583e60abb9868dfa
Alexander Chernykh <al.ed.chernykh@gmail.com>
2020-11-27 20:20:18 +0500

    (feat) remove bg transition from mobile

--- 1/scripts/background.js
+++ 1/scripts/background.js
@@ -2,8 +2,7 @@ window.addEventListener('scroll', function () {
     let background = document.querySelector('.background__mountains')
     let hills = document.querySelector('.background__hills')
     let filter = document.querySelector('.background__filter')
-    background.style.transform = "translateY(-200px)"
-    background.style.transition = "transform 2s"
+    background.classList.add('background__transform')
     hills.style.transform = "translateY(0px)"
     filter.style.transform = "translateY(0px)"
 
